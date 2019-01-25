303e34cea16cc6f276e9baa2de796c85c43ae338
unrealisher <unrealmpa3@mail.ru>
2019-01-25 18:21:14 +0300

    Updates

---
+++ precache-manifest.ef5865f4ee8615871cb7c7643eb96c00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3e018085591c3df029ff",
+    "url": "/task-3/static/js/runtime~main.3e018085.js"
+  },
+  {
+    "revision": "fb589f5f9c1bc7d23d84",
+    "url": "/task-3/static/js/main.fb589f5f.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/task-3/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "fb589f5f9c1bc7d23d84",
+    "url": "/task-3/static/css/main.618841c3.chunk.css"
+  },
+  {
+    "revision": "00cde4702591b9efd4ee6d4a7cf28b8f",
+    "url": "/task-3/index.html"
+  }
+];
---
+++ service-worker-register.js
@@ -0,0 +1,22 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", function() {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then(
+        function(registration) {
+          console.log(
+            "ServiceWorker registration succesful with scope: ",
+            registration.scope
+          );
+        },
+        function(err) {
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      )
+      .catch(function(err) {
+        console.log(err);
+      });
+  });
+} else {
+  console.log("ServiceWorker is not supported");
+}
